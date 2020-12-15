4086f3de97a0ef6a056c88ef65b8b80962063f76
Ubuntu <ubuntu@ip-172-31-31-91.eu-west-2.compute.internal>
2020-12-15 15:55:00 +0000

    Finish layout and routes (with errors and unworking header-links) 2.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'users/new'
   root             'static_pages#home' # меняет URL static_pages/home на пару контроллер-действие static_pages#home, которая обеспечивает направление запроса GET для / к действию home в контроллере StaticPages.
-  get    '/help',    to: 'static_pages#help' # Правило направляет запрос для URL /static_pages/help к действию home в контроллере StaticPages.
-  get    '/about',   to: 'static_pages#about' # Правило направляет запрос для URL /static_pages/about к действию home в контроллере StaticPages.
-  get    '/contact', to: 'static_pages#contact'
-  get    '/signup',  to: 'users#new'
+  get 'help'    => 'static_pages#help' as: static_pages_help # Правило направляет запрос для URL /static_pages/help к действию home в контроллере StaticPages.
+  get 'about'   => 'static_pages#about' # Правило направляет запрос для URL /static_pages/about к действию home в контроллере StaticPages.
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
   
 end
