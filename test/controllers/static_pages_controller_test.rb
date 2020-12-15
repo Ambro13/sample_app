9bf54c5e998debbcf88f77e89256f15a9a9a4f32
Ubuntu <ubuntu@ip-172-31-31-91.eu-west-2.compute.internal>
2020-12-15 15:51:14 +0000

    Finish layout and routes (with errors and unworking header-links) 2.0

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_path
+    static_pages_home_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App" #Этот код проверяет наличие тэга <title>, содержащего строку “Home | Ruby on Rails Tutorial Sample App”.
   end
