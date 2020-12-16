90a93a198c19a2035171c1282168c4cc5bea12cb
Ubuntu <ubuntu@ip-172-31-31-91.eu-west-2.compute.internal>
2020-12-16 13:10:18 +0000

    Maded a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  
+  root                'static_pages#home' # меняет URL static_pages/home на пару контроллер-действие static_pages#home, которая обеспечивает направление запроса GET для / к действию home в контроллере StaticPages.
+  get '/help',    to: 'static_pages#help' # Правило направляет запрос для URL /static_pages/help к действию home в контроллере StaticPages.
+  get '/about',   to: 'static_pages#about' # Правило направляет запрос для URL /static_pages/about к действию home в контроллере StaticPages.
+  get '/contact', to: 'static_pages#contact'
   get 'users/new'
-  root             'static_pages#home' # меняет URL static_pages/home на пару контроллер-действие static_pages#home, которая обеспечивает направление запроса GET для / к действию home в контроллере StaticPages.
-  get 'help'    => 'static_pages#help', as: static_pages_help # Правило направляет запрос для URL /static_pages/help к действию home в контроллере StaticPages.
-  get 'about'    => 'static_pages#about', as: static_pages_about # Правило направляет запрос для URL /static_pages/about к действию home в контроллере StaticPages.
-  get 'contact'    => 'static_pages#contact', as: static_pages_contact
   get 'signup'  => 'users#new'
   
 end
